02bf1e419003294663ca326f551c9ce3bbdad6c7
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2022-01-16 03:04:50 -0800

    Implementing the service section

---
+++ src/components/Services/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Icon1 from "../../images/svg-1.svg";
+import Icon2 from "../../images/svg-1.svg";
+import Icon3 from "../../images/svg-1.svg";
+
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP,
+} from "./ServicesElement";
+const Services = () => {
+  return (
+    <ServicesContainer>
+      <ServicesH1>Projects</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1}></ServicesIcon>
+          <ServicesH2> PLACE HOLDER DATA 1</ServicesH2>
+          <ServicesP>A lot more of place holder data</ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon2}></ServicesIcon>
+          <ServicesH2> PLACE HOLDER DATA 2</ServicesH2>
+          <ServicesP>A lot more of place holder data</ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon3}></ServicesIcon>
+          <ServicesH2> PLACE HOLDER DATA 3</ServicesH2>
+          <ServicesP>A lot more of place holder data</ServicesP>
+        </ServicesCard>
+      </ServicesWrapper>
+    </ServicesContainer>
+  );
+};
+
+export default Services;
