b9f64c6da9f33169c5db84123d0706cbecd0d87a
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2022-01-16 18:10:13 -0800

    Implementing the basic footer
    Implemented base index.js for the footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages";
+import SigninPage from "./Pages/signin";
 
 function App() {
   return (
     <Router>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/signin" element={<SigninPage />} exact />
+      </Routes>
     </Router>
   );
 }
